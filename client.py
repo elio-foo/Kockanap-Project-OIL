871f9cf66355de144c6cb9a2bef10b11397d0b91
Elio V <elio.vidhaj1@proton.me>
2026-04-24 14:56:41 +0200

    Established communication with server

---
+++ client.py
@@ -0,0 +1,44 @@
+import grpc
+import greeter_pb2
+import greeter_pb2_grpc
+
+import time
+import asyncio
+import json
+
+def message_generator():
+    for i in range(5):
+        msg = greeter_pb2.CommandMessage(
+            teamName = "ObudaInnovationLab",
+            counter = i + 1,
+            unitId = 123,
+            operation = "NOP",
+            extraJson = "{}",
+        )
+
+        print("Sending: ", msg)
+
+        yield msg
+        time.sleep(1)
+        
+def run_stream():
+    with grpc.insecure_channel("10.4.4.59:5001") as channel:
+        stub = greeter_pb2_grpc.FireRaServiceStub(channel)
+
+        # hello
+        request = greeter_pb2.HelloRequest(teamName="ObudaInnovationLab")
+        response = stub.SayHello(request)
+
+        print("Response:", response.message)
+        #
+
+        responses = stub.CommunicateWithStreams(message_generator())
+
+        for resp in responses:
+            print("Recieved: ", resp.extraJson)
+            # data = json.loads(resp.extraJson)
+            # print(json.dumps(data, indent=2))
+
+
+if __name__ == "__main__":
+    run_stream()
--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
 			packages = [
 				(pkgs.python313.withPackages (python-pkgs: with python-pkgs; [
 					grpcio
+					grpcio-tools
 				]))
 			];
 		};
