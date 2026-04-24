94cd555b4c05d2358928cd0d8006002cb6dca6e2
Elio V <elio.vidhaj1@proton.me>
2026-04-24 16:48:47 +0200

    Changes

--- client.py
+++ client.py
@@ -6,15 +6,15 @@ import time
 import asyncio
 import json
 
+class Response:
+    def __init__(self, raw_data):
+        self.teamName = raw_data.teamName
+        self.counter = raw_data.counter
+        self.operation = raw_data.operation
+        self.extraJson = raw_data.extraJson
 
-class Response():
-    pass
-
-class ServerData():
-    pass
-
-def message_generator():
-    for i in range(1):
+async def message_generator():
+    for i in range(5):
         msg = greeter_pb2.CommandMessage(
             teamName = "ObudaInnovationLab",
             counter = i + 1,
@@ -26,27 +26,25 @@ def message_generator():
         print("Sending: ", msg)
 
         yield msg
-        time.sleep(1)
+        await asyncio.sleep(1)
         
-def run_stream():
-    with grpc.insecure_channel("10.4.4.59:5001") as channel:
+async def run_stream():
+    async with grpc.aio.insecure_channel("10.4.4.59:5001") as channel:
         stub = greeter_pb2_grpc.FireRaServiceStub(channel)
 
         # hello
         request = greeter_pb2.HelloRequest(teamName="ObudaInnovationLab")
-        response = stub.SayHello(request)
+        response = await stub.SayHello(request)
 
         print("Response:", response.message)
-        #
 
-        responses = stub.CommunicateWithStreams(message_generator())
+        responses = stub.CommunicateWithStreams()
 
-        # for resp in responses:
-        #     json_resp = json.loads(resp.extraJson)
-        #     for unit in json_resp:
-                
+        async for resp in responses:
+            r = Response(resp)
 
+            print(r.teamName)
 
 
 if __name__ == "__main__":
-    run_stream()
+    asyncio.run(run_stream())
